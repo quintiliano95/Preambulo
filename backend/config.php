b640fb06a9abfbc581f41c8d03dbf8776a3b8478
Alexandre Quintiliano <98800085+quintiliano95@users.noreply.github.com>
2025-04-03 17:13:06 -0300

    banco configurado

--- backend/config.php
+++ backend/config.php
@@ -1,8 +1,8 @@
 <?php
-$host = "db";              // Nome do serviço MySQL no docker-compose
-$dbname = "preambulo_db";   // Nome do banco conforme configurado
-$username = "root";         // Usuário root
-$password = "xamds";             // Senha vazia
+$host = "db";
+$dbname = "preambulo_db";
+$username = "xamds";
+$password = "xamds";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
